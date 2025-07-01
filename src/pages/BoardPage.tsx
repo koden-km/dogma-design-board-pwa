a41506034edadf2334bbbb865d636ff63f0ff155
Kevin Millar <koden.km@gmail.com>
2025-07-01 11:48:53 +1000

    Add initial BoardPage

---
+++ src/pages/BoardPage.tsx
@@ -0,0 +1,30 @@
+import Card from "../features/card/Card.tsx";
+
+export default function BoardPage() {
+  return (
+    <div>
+      <div>Board Page</div>
+
+      <div>
+        <Card type="event" comment="Some event comment" />
+        <Card type="command" />
+        <Card type="aggregate" title="Thing" comment="Some event comment" />
+        <Card
+          type="process"
+          title="Some Process"
+          comment="What happens when the comment is extra long, because sometimes that will be useful for adding context to the card. The long comment could have multiple sentences and just be king of chunky in general."
+        />
+        <Card
+          type="process"
+          title="Some Other Process With A Longer Name and Long Comment"
+          comment="What happens when the comment is extra long, because sometimes that will be useful for adding context to the card. The long comment could have multiple sentences and just be king of chunky in general."
+        />
+        <Card
+          type="projection"
+          title="A Projection With a Really Long Name For Testing Purposes"
+          comment="Some event comment"
+        />
+      </div>
+    </div>
+  );
+}
